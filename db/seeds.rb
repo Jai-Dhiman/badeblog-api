f09e61b792f9f04dddf5959176cd4f3bfc748017
JaiDhiman <jaidhiman2000@gmail.com>
2024-11-15 17:37:48 -0800

    database config

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,31 +1,28 @@
-# admin = User.find_by(email: 'admin@gmail.com')
+user = User.new(
+  name: 'PR Dhiman',
+  email: 'prdhiman@gmail.com',
+  password: 'kalakunj101',
+  password_confirmation: 'kalakunj101',
+  role: 'admin'
+)
+user.save!
 
-# unless admin
-#   User.create!(
-#     name: 'admin',
-#     email: 'admin@gmail.com',
-#     password: 'password',
-#     password_confirmation: 'password',
-#     role: 'admin'
-#   )
-#   puts 'Admin user created successfully!'
-# else
-#   puts 'Admin user already exists'
-# end
+categories = [
+  Category.new(name: 'Essays'),
+  Category.new(name: 'Articles'),
+  Category.new(name: 'Short Stories'),
+  Category.new(name: 'Poems'),
+  Category.new(name: 'Plays'),
+  Category.new(name: 'Other')
+]
 
-# categories = Category.create!([
-#   { name: 'Stories' },
-#   { name: 'Articles' },
-#   { name: 'Blogs' }
-# ])
+categories.each(&:save!)
 
 story = Story.new(
-  title: 'Sample Story 1',
-  content: 'This is a sample 1',
+  title: 'Sample Story',
+  content: 'This is a sample',
   status: 'published',
-  # category: categories.first,
+  category: categories.first,
   user_id: 1
 )
-
 story.save!
-
