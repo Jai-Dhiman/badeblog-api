d577f07595ea6caf94d0960f5abe0d3b1e57b514
JaiDhiman <jaidhiman2000@gmail.com>
2024-12-28 03:19:24 -0800

    debugging

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,4 +1,6 @@
 class SubscribersController < ApplicationController
+  before_action :ensure_frontend_url
+
   def create
     subscriber = Subscriber.new(subscriber_params)
     if subscriber.save
@@ -10,9 +12,12 @@ class SubscribersController < ApplicationController
   end
 
   def unsubscribe
+    Rails.logger.info "FRONTEND_URL: #{ENV['FRONTEND_URL']}"
     @subscriber = Subscriber.find_by!(unsubscribe_token: params[:token])
     if @subscriber.destroy
-      redirect_to "#{ENV['FRONTEND_URL']}?unsubscribed=true"
+      redirect_url = "#{ENV['FRONTEND_URL']}?unsubscribed=true"
+      Rails.logger.info "Redirecting to: #{redirect_url}"
+      redirect_to redirect_url
     else
       redirect_to "#{ENV['FRONTEND_URL']}?unsubscribe_error=true"
     end
@@ -25,4 +30,9 @@ class SubscribersController < ApplicationController
   def subscriber_params
     params.require(:subscriber).permit(:email)
   end
+
+  def ensure_frontend_url
+    unless ENV['FRONTEND_URL'].present?
+      Rails.logger.error "FRONTEND_URL environment variable is not set!"
+    end
 end
