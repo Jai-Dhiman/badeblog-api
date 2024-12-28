9ec63a4bbfce454e5d15d1bc1309be3b7287b20d
JaiDhiman <jaidhiman2000@gmail.com>
2024-12-28 00:27:33 -0800

    fixed broken controller name

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,14 +1,20 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-  provider :google_oauth2, ENV['GOOGLE_CLIENT_ID'], ENV['GOOGLE_CLIENT_SECRET'], {
-    scope: 'email,profile',
-    prompt: 'select_account',
-    image_aspect_ratio: 'square',
-    image_size: 50,
-    provider_ignores_state: true, 
-    access_type: 'offline',
-    skip_jwt: true,
-    redirect_uri: "#{ENV['FRONTEND_URL']}/auth/callback"
-  }
+  provider :google_oauth2, 
+    ENV['GOOGLE_CLIENT_ID'],
+    ENV['GOOGLE_CLIENT_SECRET'],
+    {
+      scope: 'email,profile',
+      prompt: 'select_account',
+      image_aspect_ratio: 'square',
+      image_size: 50,
+      redirect_uri: proc { |env| 
+        if Rails.env.production?
+          "https://web-production-e3d6.up.railway.app/auth/google_oauth2/callback"
+        else
+          "http://localhost:3000/auth/google_oauth2/callback"
+        end
+      }
+    }
 end
 
 OmniAuth.config.allowed_request_methods = [:post, :get]
