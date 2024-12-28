fc6228c939f0a33b1ecedae8e3945fd048c3dcbf
JaiDhiman <jaidhiman2000@gmail.com>
2024-12-28 03:27:21 -0800

    debugging

--- app/controllers/subscribers_controller.rb
+++ app/controllers/subscribers_controller.rb
@@ -1,6 +1,4 @@
 class SubscribersController < ApplicationController
-  before_action :ensure_frontend_url
-
   def create
     subscriber = Subscriber.new(subscriber_params)
     if subscriber.save
@@ -12,28 +10,20 @@ class SubscribersController < ApplicationController
   end
 
   def unsubscribe
-    Rails.logger.info "FRONTEND_URL: #{ENV['FRONTEND_URL']}"
     @subscriber = Subscriber.find_by!(unsubscribe_token: params[:token])
     if @subscriber.destroy
       redirect_url = "#{ENV['FRONTEND_URL']}?unsubscribed=true"
-      Rails.logger.info "Redirecting to: #{redirect_url}"
-      redirect_to redirect_url
+      redirect_to redirect_url, allow_other_host: true
     else
-      redirect_to "#{ENV['FRONTEND_URL']}?unsubscribe_error=true"
+      redirect_to "#{ENV['FRONTEND_URL']}?unsubscribe_error=true", allow_other_host: true
     end
   rescue ActiveRecord::RecordNotFound
-    redirect_to "#{ENV['FRONTEND_URL']}?unsubscribe_error=invalid_token"
+    redirect_to "#{ENV['FRONTEND_URL']}?unsubscribe_error=invalid_token", allow_other_host: true
   end
   
   private
 
   def subscriber_params
     params.require(:subscriber).permit(:email)
-  end
-
-  def ensure_frontend_url
-    unless ENV['FRONTEND_URL'].present?
-      Rails.logger.error "FRONTEND_URL environment variable is not set!"
-    end
   end
 end
